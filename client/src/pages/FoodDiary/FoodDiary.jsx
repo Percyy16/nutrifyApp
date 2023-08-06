4fb75cc2ff8764b906725c55bc5d32a38843a4b5
Percyy16 <marktyban@gmail.com>
2023-08-06 15:25:05 +0200

    FooDiary page design

--- client/src/pages/FoodDiary/FoodDiary.jsx
+++ client/src/pages/FoodDiary/FoodDiary.jsx
@@ -3,19 +3,54 @@ import "../FoodDiary/FoodDiary.scss";
 
 export const FoodDiary = () => {
     return (
-    <div className="foodDiary_container">
-        <h1>
-            Food Diary
-        </h1>
-        <div className="foodDiary_container__breakfast">
-
-        </div>
-        <div className="foodDiary_container__lunch">
-
-        </div>
-        <div className="foodDiary_container__dinner">
-        
-        </div>
-    </div>
+        <>
+            <div className="foodDiary_container__header">
+                <h1>
+                    Get Your Daily Calories Under Control 💪
+                </h1>
+            </div>
+            <div className="foodDiary_container__section">
+                <div className="foodDiary_container__food">
+                    <div>
+                        <p>Breakfast</p>
+                    </div>
+                    <div>
+                        <button>
+                            Add Food
+                        </button>
+                    </div>
+                </div>
+                <div className="foodDiary_container__food">
+                    <div>
+                        <p>Lunch</p>
+                    </div>
+                    <div>
+                        <button>
+                            Add Food
+                        </button>
+                    </div>
+                </div>
+                <div className="foodDiary_container__food">
+                    <div>
+                        <p>Dinner</p>
+                    </div>
+                    <div>
+                        <button>
+                            Add Food
+                        </button>
+                    </div>
+                </div>
+                <div className="foodDiary_container__food">
+                    <div>
+                        <p>Snacks</p>
+                    </div>
+                    <div>
+                        <button>
+                            Add Food
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+import "../Home/Home.scss";
+
 export const Home = () => {
     return (
-    <div>Home</div>
+        <div className="home_container">
+            <div className="home_container__whyUse">
+                <h1>
+                    Why to use Nutrify?
+                </h1>
+                <p>
+                    Track your daily calories
+                </p>
+                <p>
+                    Schedule your workouts
+                </p>
+            </div>
+        </div>
     );
 };
