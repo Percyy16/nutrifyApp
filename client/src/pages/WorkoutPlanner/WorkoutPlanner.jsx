cbe37cc14361bc7d5348f54bb60259b99c88bd4c
Percyy16 <marktyban@gmail.com>
2023-08-12 20:52:36 +0200

    workoutPlanner starting design

--- client/src/pages/WorkoutPlanner/WorkoutPlanner.jsx
+++ client/src/pages/WorkoutPlanner/WorkoutPlanner.jsx
@@ -1,5 +1,14 @@
+import React from "react";
+import "../WorkoutPlanner/WorkoutPlanner.scss";
+
 export const WorkoutPlanner = () => {
     return (
-    <div>WorkoutPlanner</div>
+        <>
+            <div className="workoutPlanner_container__header">
+                <h1>
+                    Plan Your Workouts 💪
+                </h1>
+            </div>
+        </>
     );
 };
